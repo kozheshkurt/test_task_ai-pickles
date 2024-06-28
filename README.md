720fcd48c0cc96adff55bacac7a7074b3f9c5d0f
kozheshkurt <vkozeskurt@gmail.com>
2024-06-28 17:59:27 +0300

    add test case in summarizer.py

--- README.md
+++ README.md
@@ -1,16 +1,28 @@
 # test_task_ai-pickles
 
-Setup
-Create a virtual environment:
+## Setup
 
-python -m venv env
-source env/bin/activate  # On Windows use `env\Scripts\activate`
-Install dependencies:
+1. Create a virtual environment:
+   ```
+   python -m venv env
+   source env/bin/activate  # On Windows use `env\Scripts\activate`
+   ```
 
-pip install fastapi uvicorn langchain
-Run the application:
+2. Install dependencies:
+   ```bash
+   pip install fastapi uvicorn langchain langchain_huggingface
+   ```
 
-uvicorn main:app --reload
-Test the endpoint:
+3. To perform authorization in HuggingFace creat file 'auth.py', get the Hugging Face Access Token (https://huggingface.co/docs/hub/security-tokens) and paste it into the 'auth.py':
+   ```
+   import os
+   os.environ['HUGGINGFACEHUB_API_TOKEN'] = "hf_.............."
+   ```
 
-Send a POST request to http://127.0.0.1:8000/summarize with a JSON body containing the text to be summarized.
+4. Run the application:
+   ```bash
+   uvicorn main:app --reload
+   ```
+
+5. Test the endpoint:
+   - Send a POST request to `http://127.0.0.1:8000/summarize` with a JSON body containing the text to be summarized.
--- summarizer.py
+++ summarizer.py
@@ -21,3 +21,13 @@ class SimpleSummarizer():
         summary = self.llm.invoke(messages)
         return summary
 
+
+# test case if the script is called directly
+if __name__ == "__main__":
+    summarizer = SimpleSummarizer("google/flan-t5-small")
+    test_text = '''
+    Introduction. The liver is known to be one of the central organs of metabolic regulation, as it provides both synthesis of a significant number of macromolecules and modification, decomposition and detoxification of a lot of metabolites and xenobiotics. Disruption of the balance between these processes in the liver is usually accompanied by changes in its structural and functional characteristics, and in case of long-term deviations from the homeostatic norm, pathological processes take place.  Pathological changes in the liver can be induced by extremely various biological (viruses, bacteria), physical and chemical factors.
+    Pathological processes in the liver, independent of the pathogenetic factor, go through a number of consistent stages: hepatitis, fibrosis, cirrhosis and/or carcinogenesis. Currently, mortality from liver diseases ranks 5th in the world. The study of the mechanisms of formation and development of liver fibrosis is the basis for the development of ways to prevent and treat liver diseases. Such fundamental studies can be carried out only on experimental animal models. Although there is an intensive search for modern methods of evaluating the functional state of the liver, morphological studies are still the gold standard in hepatological diagnostics. However, establishing a diagnosis at early or initial stages, significant changes in structural and functional characteristics that may lead to fibrosis in the future is very difficult, and most often impossible.
+    '''
+    test_summary = summarizer.summarize(test_text)
+    print(test_summary)

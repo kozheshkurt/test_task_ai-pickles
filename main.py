01004f6839719fc3d821f87aa6c468d62731f912
kozheshkurt <vkozeskurt@gmail.com>
2024-06-28 18:39:24 +0300

    add comments

--- main.py
+++ main.py
@@ -2,25 +2,27 @@ from fastapi import FastAPI
 import uvicorn
 from pydantic import BaseModel
 
-from summarizer import SimpleSummarizer
+from summarizer import SimpleSummarizer #class, created in different file
 
 
+# describe the model of data, get from post request
 class Request(BaseModel):
     initial_text: str
 
 
 app = FastAPI()
 
+# variable with the used model to have a possibility of using different models
 model_id = "google/flan-t5-small"
 
 
 @app.post("/summarize")
 async def create_summary(request: Request):
-    text_to_summarize = request.initial_text
-    summarizer = SimpleSummarizer(model_id)
-    summary = summarizer.summarize(text_to_summarize)
+    text_to_summarize = request.initial_text    #get text from request
+    summarizer = SimpleSummarizer(model_id)     #create an instance of SimpleSummarizer class from the file summarizer.py
+    summary = summarizer.summarize(text_to_summarize)  #get the summary by summarize method of SimpleSummarizer class
     return {"summary": summary}
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="127.0.0.1", port=8000) #run the server
--- summarizer.py
+++ summarizer.py
@@ -1,10 +1,12 @@
 from langchain_core.messages import HumanMessage, SystemMessage
 from langchain_huggingface import HuggingFacePipeline
-import auth
+#run file auth.py, which should be created for authorization in Hugging Face, according to readme instruction
+import auth 
 
 class SimpleSummarizer():
 
     def __init__(self, model_id: str):
+        #parameter is model_id, such as "google/flan-t5-small", attribute llm is created while creating an instance
         self.llm = HuggingFacePipeline.from_model_id(
             model_id = model_id,
             task = "summarization",
@@ -12,22 +14,30 @@ class SimpleSummarizer():
 
 
     def summarize(self, text_to_summarize: str):
-        
+        # the method takes text and returns the summary of this text
+
+        # prompt with system instruction and text to process
         messages = [
             SystemMessage(content="Write a concise summary of the following:"),
             HumanMessage(content=text_to_summarize),
         ]
 
+        # run llm with the message
         summary = self.llm.invoke(messages)
         return summary
 
 
 # test case if the script is called directly
 if __name__ == "__main__":
+    
+    # create an instance of SimpleSummarizer
     summarizer = SimpleSummarizer("google/flan-t5-small")
+    
     test_text = '''
     Introduction. The liver is known to be one of the central organs of metabolic regulation, as it provides both synthesis of a significant number of macromolecules and modification, decomposition and detoxification of a lot of metabolites and xenobiotics. Disruption of the balance between these processes in the liver is usually accompanied by changes in its structural and functional characteristics, and in case of long-term deviations from the homeostatic norm, pathological processes take place.  Pathological changes in the liver can be induced by extremely various biological (viruses, bacteria), physical and chemical factors.
     Pathological processes in the liver, independent of the pathogenetic factor, go through a number of consistent stages: hepatitis, fibrosis, cirrhosis and/or carcinogenesis. Currently, mortality from liver diseases ranks 5th in the world. The study of the mechanisms of formation and development of liver fibrosis is the basis for the development of ways to prevent and treat liver diseases. Such fundamental studies can be carried out only on experimental animal models. Although there is an intensive search for modern methods of evaluating the functional state of the liver, morphological studies are still the gold standard in hepatological diagnostics. However, establishing a diagnosis at early or initial stages, significant changes in structural and functional characteristics that may lead to fibrosis in the future is very difficult, and most often impossible.
     '''
+    
+    # use summarize method of SimpleSummarizer class for test_text
     test_summary = summarizer.summarize(test_text)
     print(test_summary)
